1f306fb15081f865daa4934237044a951baf44a7
Jesus Christian Castillo <christian.castillo.lozano@gmail.com>
2020-06-11 00:21:05 -0500

    Update conection  of the database

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,11 +1,7 @@
 <?php
-
 require 'vendor/autoload.php';
-
-
-
 session_start();
-/*
+
 $dbUrl=$_ENV["CLEARDB_DATABASE_URL"];
 $url = parse_url($dbUr);
 
@@ -16,17 +12,17 @@ $db = substr($url["path"], 1);
 
 $mysqli = new mysqli($server, $username, $password, $db);
 if(isset($mysqli)){
-	echo 'Conexion Fallida : ', mysqli_connect_error();
-	exit();
-}*/
-
+	echo 'Conexión Fallida en db : ', mysqli_connect_error();
+	
+}
+/*
 $mysqli=new mysqli("localhost","root","","login"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 	
 	if(mysqli_connect_error()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}
-
+*/
 	
 ?>
 
